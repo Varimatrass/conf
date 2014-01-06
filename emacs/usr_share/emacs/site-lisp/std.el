434d53aea5ec277c33c00f75c062b0a398b2260d
Matthias De Jesus Oliveira <mde-jesu@student.42.fr>
2014-01-06 14:59:10 +0100

    42 conf

---
+++ emacs/usr_share/emacs/site-lisp/std.el
@@ -0,0 +1,21 @@
+;;
+;; std.el for emacs conf in site-local
+;; 
+;; Made by Nicolas Sadirac
+;; Login   <ns@42.fr>
+;; 
+;; Started on  Sun Oct 17 02:20:57 1993 Nicolas Sadirac
+;; Last update Thu Jul  8 12:47:38 2013 root
+;;
+
+(defun do_insert_time () 
+  (interactive)
+ (insert-string (current-time-string)))
+(set-variable 'c-argdecl-indent   0)
+
+
+(normal-erase-is-backspace-mode 0)
+
+(global-set-key "" 'backward-delete-char)
+(global-set-key "" 'compile)
+(global-set-key "" 'goto-line)
