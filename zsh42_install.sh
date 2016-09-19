97cd247102d8d6023677ea2644023746c3ee8275
Matthias De Jesus Oliveira <mde-jesu@student.42.fr>
2016-09-19 11:20:12 +0200

    conf pour 42 OK

--- zsh42_install.sh
+++ zsh42_install.sh
@@ -1,34 +1,28 @@
 #!/bin/bash
 
-apt-get install git
-apt-get install emacs-nox
-apt-get install vim
-apt-get install gcc
-apt-get install g++
-
 # ------------------ vars ---------------
 trucs="\nif [[ -f ~/$myrc ]]; then\n  source ~/.$myrc\nfi\n\nUSER='mde-jesu'\nexport USER\nMAIL=\"\$USER@student.42.fr\"\nexport MAIL\n"
 
 trucsvim="\nif filereadable(\"$HOME/.vimrc\")\n  source $HOME/.vimrc\nendif\n"
 
-rc=".bashrc"
-myrc=".mybashrc"
+rc=".zshrc"
+myrc=".myzshrc"
 
 # --------------------------------------
 
 #echo de $truc dans rc
-echo -e $trucs >> ~/$rc
+#echo -e $trucs >> ~/$rc
 
 #liens symbolique pour myrc
-ln -s ~/conf/bash/$myrc ~/$myrc
+ln -s ~/conf/zsh/$myrc ~/$myrc
 
 #liens symbolique de .gitconfig et .gitignore_global
 ln -s ~/conf/gitconf/.gitconfig ~/.gitconfig
 ln -s ~/conf/gitconf/.gitignore_global ~/.gitignore_global
 
 #echo du source vimrc dans vimrc
-echo -e $trucsvim >> /etc/vim/vimrc
+#echo -e $trucsvim >> /etc/vim/vimrc
 
 #liens symbolique de vimrc et myvimrc
-ln -s ~/conf/vim/vimrc ~/.vimrc
-ln -s ~/conf/vim/.myvimrc ~/.myvimrc
+#ln -s ~/conf/vim/vimrc ~/.vimrc
+ln -s ~/conf/vim/myvimrc_42 ~/.myvimrc
